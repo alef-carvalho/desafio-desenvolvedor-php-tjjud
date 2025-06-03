c885433fd0bf5306d5e812cf21df619466ac7a5e
Alef Carvalho <csilva.alef@gmail.com>
2025-06-03 13:15:36 -0400

    Update AuthController.php

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -20,7 +20,7 @@ class AuthController extends Controller
     }
 
     #[Post('/')]
-    public function logi(LoginFormRequest $request)
+    public function login(LoginFormRequest $request)
     {
         $credentials = $request->validated();
         $token = $this->service->login($credentials);
